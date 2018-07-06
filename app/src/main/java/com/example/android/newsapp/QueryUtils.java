46782bb39686eb08269d8f93e12378aef9869be8
Chris <chrisabbod@gmail.com>
2018-07-06 18:27:45 -0400

    <feat>: Create QueryUtils Helper Class for Extracting API Data

---
+++ app/src/main/java/com/example/android/newsapp/QueryUtils.java
@@ -0,0 +1,18 @@
+package com.example.android.newsapp;
+
+/**
+ * Helper methods for requesting/receiving data from The Guardian
+ */
+public final class QueryUtils {
+
+    /**
+     * Create a private constructor because no one will ever need to create an object.
+     * This class is only meant to hold static variables and methods, which can be accessed
+     * directly fro the class name QueryUtils
+     */
+    private QueryUtils(){
+        //No need to ever create an object
+    }
+
+
+}
