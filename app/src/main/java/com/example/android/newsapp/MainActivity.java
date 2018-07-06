a4e9664d340a0a8491c582e06ffba40f33c32ef3
Chris <chrisabbod@gmail.com>
2018-07-06 17:16:14 -0400

    <feat>: Change activity_main.xml to ListView

--- app/src/main/java/com/example/android/newsapp/MainActivity.java
+++ app/src/main/java/com/example/android/newsapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String guardian_request_url = "https://content.guardianapis.com/search?q=politics&api-key=36f66e45-37ec-4793-9f1e-452f9718c623";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
