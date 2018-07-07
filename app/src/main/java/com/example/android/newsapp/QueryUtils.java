c23ebe249b2e11cdd80f4e1fa9e1ad80ca35acb2
Chris <chrisabbod@gmail.com>
2018-07-06 20:24:22 -0400

    <feat>: Create readFromStream Function in QueryUtils Class

--- app/src/main/java/com/example/android/newsapp/QueryUtils.java
+++ app/src/main/java/com/example/android/newsapp/QueryUtils.java
@@ -7,8 +7,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -103,5 +110,21 @@ public final class QueryUtils {
         return url;
     }
 
+    /**
+     * Convert the InputStream into a String which contains the whole JSON response from the server
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
 
 }
